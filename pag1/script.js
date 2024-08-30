1c1e190f4208a6366c85258d6ca9e2978c60e3c4
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2024-08-30 11:17:36 -0300

    tela inicial arrumada(paragrafo e menu) - Murilo

--- pag1/script.js
+++ pag1/script.js
@@ -2,7 +2,7 @@
 const target = document.querySelectorAll('[data-anime="right"]');
 const animationClass = "animation";
 function animeScroll() {
-    const windowTop1 = window.pageYOffset + 100;
+    const windowTop1 = window.pageYOffset + 170;
     target.forEach(function(element){
         if(windowTop1 > element.offsetTop){
             element.classList.add("animation");
