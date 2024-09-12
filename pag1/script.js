d0675c32e0742d5d2f6bfe9065855cb7cf782efd
Erick-Henrique-Pedrini <erickpedrinebr@gmail>
2024-09-12 11:20:23 -0300

    Mudanças sec2

--- pag1/script.js
+++ pag1/script.js
@@ -1,19 +1,37 @@
-/*animação*/
-const target = document.querySelectorAll('[data-anime="right"]');
-const animationClass = "animation";
+// Seleciona os elementos que serão animados
+const targetsRight = document.querySelectorAll('[data-anime="right"]');
+const targetsDown = document.querySelectorAll('[data-anime="down"]');
+
+// Função para animar os elementos da esquerda para a direita
 function animeScroll() {
-    const windowTop1 = window.pageYOffset + 170;
-    target.forEach(function(element){
-        if(windowTop1 > element.offsetTop){
+    const windowTop = window.pageYOffset + 170;
+    console.log("Window Top:", windowTop); // Debugging
+    targetsRight.forEach(function(element) {
+        console.log("Right Element:", element); // Debugging
+        if (windowTop > element.offsetTop) {
             element.classList.add("animation");
-        }
-        else{
+        } else {
             element.classList.remove("animation");
         }
-    })
-
+    });
 }
-window.addEventListener('scroll', function(){
-    animeScroll();
-})
 
+// Função para animar os elementos de cima para baixo
+function animeScroll2() {
+    const windowTop2 = window.pageYOffset + -600;
+    console.log("Window Top for Down:", windowTop2); // Debugging
+    targetsDown.forEach(function(element) {
+        console.log("Down Element:", element); // Debugging
+        if (windowTop2 > element.offsetTop) {
+            element.classList.add("animation2");
+        } else {
+            element.classList.remove("animation2");
+        }
+    });
+}
+
+// Adiciona o event listener para o evento scroll
+window.addEventListener('scroll', function() {
+    animeScroll();
+    animeScroll2();
+});
