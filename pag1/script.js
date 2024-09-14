368a78a5856855ac105ca2450484170eeec222e2
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2024-09-14 10:48:39 -0300

    Mudança

--- pag1/script.js
+++ pag1/script.js
@@ -1,7 +1,8 @@
 
 const targetsRight = document.querySelectorAll('[data-anime="right"]');
 const targetsDown = document.querySelectorAll('[data-anime="down"]');
-
+const targetsLW = document.querySelectorAll('[data-anime="LW"]');
+const targetsRU = document.querySelectorAll('[data-anime="RU"]');
 
 function animeScroll() {
     const windowTop = window.pageYOffset + 170;
@@ -29,9 +30,34 @@ function animeScroll2() {
         }
     });
 }
-
+function animeScroll3() {
+    const windowTop3 = window.pageYOffset + -3000;
+    console.log("Window Top for Down:", windowTop3); 
+    targetsLW.forEach(function(element) {
+        console.log("Left Down Element:", element); 
+        if (windowTop3 > element.offsetTop) {
+            element.classList.add("animation3");
+        } else {
+            element.classList.remove("animation3");
+        }
+    });
+}
+function animeScroll4() {
+    const windowTop4 = window.pageYOffset + -2800;
+    console.log("Window Top for Down:", windowTop4); 
+    targetsRU.forEach(function(element) {
+        console.log("Right Up Element:", element); 
+        if (windowTop4 > element.offsetTop) {
+            element.classList.add("animation4");
+        } else {
+            element.classList.remove("animation4");
+        }
+    });
+}
 
 window.addEventListener('scroll', function() {
     animeScroll();
     animeScroll2();
+    animeScroll3() ;
+    animeScroll4();
 });
