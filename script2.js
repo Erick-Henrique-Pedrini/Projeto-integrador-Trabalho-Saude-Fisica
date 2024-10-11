670cb3d29f7b691fd2d77d0fef83730c29ad596c
murilojamoski <mugames2002@gmail.com>
2024-10-11 10:21:49 -0300

    js arrumando

--- script2.js
+++ script2.js
@@ -6,33 +6,45 @@ document.querySelectorAll('input').forEach((input, index) => {
         if (nextInput) nextInput.focus();
       }
     });
-  });
-  const altura = document.getElementById('altura')
-  const peso = document.getElementById('peso')
-  const nome = document.getElementById('nome');
-  const esporte = document.getElementsByName('exercicio');
-  const lesao = document.getElementsByName('lesao');
-  const btn = document.getElementById('button');
-  
-  btn.addEventListener('click', function() {
-      let esporteSelecionado = '';
-      let lesaoSelecionada = '';     
-      for (let i = 0; i < esporte.length; i++) {
-          if (esporte[i].checked) {
-              esporteSelecionado = esporte[i].value
-          }
-      }
-      for (let i = 0; i < lesao.length; i++) {
-          if (lesao[i].checked) {
-              lesaoSelecionada = lesao[i].value;
-          }
-      }
-      localStorage.setItem('esporteSelecionado', esporteSelecionado);
-      localStorage.setItem('lesaoSelecionada', lesaoSelecionada);
-      
-      alert('Informações salvas! Navegue até a página de resultados.');
-      
-      nome.value = ""
-      peso.value = ""
-      altura.value = ""
-  });
+});
+
+const altura = document.getElementById('altura');
+const peso = document.getElementById('peso');
+const nome = document.getElementById('nome');
+const esporte = document.getElementsByName('exercicio');
+const lesao = document.getElementsByName('lesao');
+const btn = document.getElementById('button');
+
+btn.addEventListener('click', function() {
+
+    if (altura.value === '' || peso.value === '' || nome.value === '') {
+        alert('Por favor, preencha todos os campos obrigatórios.');
+        return; 
+    }
+
+    let esporteSelecionado = '';
+    let lesaoSelecionada = '';     
+    for (let i = 0; i < esporte.length; i++) {
+        if (esporte[i].checked) {
+            esporteSelecionado = esporte[i].value;
+        }
+    }
+    for (let i = 0; i < lesao.length; i++) {
+        if (lesao[i].checked) {
+            lesaoSelecionada = lesao[i].value;
+        }
+    }
+
+    
+    if (esporteSelecionado === '' || lesaoSelecionada === '') {
+        alert('Por favor, selecione pelo menos um esporte e uma lesão.');
+        return; 
+    }
+
+    localStorage.setItem('esporteSelecionado', esporteSelecionado);
+    localStorage.setItem('lesaoSelecionada', lesaoSelecionada); 
+    nome.value = "";
+    peso.value = "";
+    altura.value = "";
+    window.location.href = "http://127.0.0.1:3000/index.html#";
+});
